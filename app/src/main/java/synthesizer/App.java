edce6d77bf1d47d35ffb697728e6e40043159db7
joonaskiuru <joonas.kiuru@gmail.com>
2024-02-26 15:28:04 +0200

    feat: Working demo for A,S,D - keys

--- app/src/main/java/synthesizer/App.java
+++ app/src/main/java/synthesizer/App.java
@@ -16,34 +16,9 @@ public class App {
 
     public static void main(String[] args) {
 
-        // Creating instance of JFrame
-        JFrame frame = new JFrame();
 
         Synth synth = new Synth();
-        synth.play(60,127);
-
-
-
-        // Creating instance of JButton
-        JButton button = new JButton("Test Button");
-
-        // x axis, y axis, width, height
-        button.setBounds(150, 200, 220, 50);
-
-        // adding button in JFrame
-        frame.add(button);
-
-        // 400 width and 500 height
-        frame.setSize(400, 400);
-
-        // using no layout managers
-        frame.setLayout(null);
-
-        // making the frame visible
-        frame.setVisible(true);
-
-        // exit the application when closing from GUI
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
 
 }
--- app/src/main/java/synthesizer/Synth.java
+++ app/src/main/java/synthesizer/Synth.java
@@ -1,11 +1,16 @@
 package synthesizer;
 
 import javax.sound.midi.*;
+import javax.swing.*;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 
-public class Synth {
+public class Synth extends JFrame implements KeyListener  {
 
     private static final Logger LOG = LogManager.getLogger(); // LOGGER
     public static Synthesizer synth;
@@ -32,19 +37,65 @@ public class Synth {
         } catch (MidiUnavailableException e) {
             LOG.error(e);
         }
+
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        setVisible(true);
+
+        // exit the application when closing from GUI
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void play(int note, int velocity) {
         channels[0].noteOn(note, velocity);
 
-        try {
-            // Hold the note for 2 seconds
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            LOG.error(e);
-        }
+        // Turn the note off
+        channels[0].noteOff(note);
+    }
+
+    public static void stop(int note) {
 
         // Turn the note off
         channels[0].noteOff(note);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyChar()){
+            case 'a':
+                System.out.println("A");
+                play(60,127);
+                break;
+            case 's':
+                play(62,127);
+                break;
+            case 'd':
+                play(64,127);
+                break;
+
+
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch (e.getKeyChar()){
+            case 'a':
+                stop(60);
+                break;
+            case 's':
+                stop(62);
+                break;
+            case 'd':
+                stop(64);
+                break;
+        }
     }
 }

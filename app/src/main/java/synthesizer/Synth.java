524cfc9eb06d388f4b8bd134f681708349374c55
joonaskiuru <joonas.kiuru@gmail.com>
2024-03-08 11:12:34 +0200

    feat: correct notes on corresponding keys

--- app/src/main/java/synthesizer/Synth.java
+++ app/src/main/java/synthesizer/Synth.java
@@ -10,7 +10,9 @@ import org.apache.logging.log4j.Logger;
 import java.awt.event.ActionEvent;
 import java.awt.geom.*;
 import java.awt.*;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
 
 public class Synth extends JFrame  {
 
@@ -26,7 +28,17 @@ public class Synth extends JFrame  {
     public static boolean sPressed = false;
     public static boolean dPressed = false;
 
+    private static Map<String, JButton> noteButtons = new HashMap<>();
+    private static Map<String, Integer> keyValues = new HashMap<>();
+    private static Map<String, Boolean> pressedBooleans = new HashMap<>();
+
     public static final int IFW = JComponent.WHEN_IN_FOCUSED_WINDOW;
+
+        // Array of white key labels (notes)
+    public static final String[] WHITEKEYS = {"C", "D", "E", "F", "G", "A", "B"};
+
+    // Array of black key labels (notes)
+    public static final String[] BLACKKEYS = {"C#", "D#", "", "F#", "G#", "A#", ""};
 
     public Synth() {
 
@@ -50,119 +62,101 @@ public class Synth extends JFrame  {
         }
 
         // Set size of the frame
-        this.setSize(300,300);
-        this.setLayout(new FlowLayout());
-
+        // this.setSize(300,300);
+        this.setLayout(new BorderLayout());
 
         // Create panel
-        JPanel panel = new JPanel();
-        add(panel);
+        JPanel whiteKeysPanel = new JPanel();
+        whiteKeysPanel.setLayout(new GridLayout(1,7));
 
-        // Create Button for C note, set size & add to frame
-        JButton aButton = new JButton("C");
-        aButton.setPreferredSize(new Dimension(50,200));
-        add(aButton);
-
-        // Create Button for D note, set size & add to frame
-        JButton sButton = new JButton("D");
-        sButton.setPreferredSize(new Dimension(50,200));
-        add(sButton);
-
-        // Create Button for E note, set size & add to frame
-        JButton dButton = new JButton("E");
-        dButton.setPreferredSize(new Dimension(50,200));
-        add(dButton);
+        // Create panel
+        JPanel blackKeysPanel = new JPanel();
+        blackKeysPanel.setLayout(null);
+        //blackKeysPanel.setPreferredSize(new Dimension(100,200));
+        blackKeysPanel.setOpaque(false);
 
 
 
-        // A Inputs
-        bindActions(panel, "A",
-            // Action for pressed key
-            new AbstractAction() {
-                public void actionPerformed(ActionEvent e) {
-                    if (!aPressed) {
-                        aPressed = true;
-                        channels[0].noteOn(60, VELOCITY);
-                        aButton.setBackground(Color.GRAY);
-                    }
-                }
-            },
-            // Action for released key
-            new AbstractAction() {
-                public void actionPerformed(ActionEvent e) {
-                    channels[0].noteOff(60);
-                    aPressed = false;
-                    aButton.setBackground(Color.WHITE);
-                }
+        // Keyboard keys that activate white keys
+        String[] pressedKeysWhite = {"A","S","D","F","G","H","J"};
+
+        // Keyboard keys that activate black keys
+        String[] pressedKeysBlack = {"W","E","T","Y","U"};
+
+        // Create buttons for each note (white keys)
+        for(String note : WHITEKEYS) {
+            JButton button = new JButton(note); // Create a button for a key
+            button.setPreferredSize(new Dimension(100,350)); // Set size for the key
+            button.setBackground(Color.WHITE);
+            button.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            button.setVerticalAlignment(SwingConstants.BOTTOM);
+            button.setMargin(new Insets(10, 10, 20, 10));
+            whiteKeysPanel.add(button);
+
+            // Place the button into a HashMap with it's corresponding note serving as key
+            noteButtons.put(note, button);
+        }
+
+        // Create buttons for each note (black keys)
+        int offset = 50;
+        for(String note : BLACKKEYS) {
+            if(!note.isEmpty()) {
+                JButton button = new JButton(note);
+                button.setBounds(offset,0,100,175);
+                button.setBackground(Color.BLACK);
+                button.setMargin(new Insets(10, 10, 10, 10));
+                button.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+                blackKeysPanel.add(button);
+                // Place the button into a HashMap with it's corresponding note serving as key
+                noteButtons.put(note, button);
             }
-        );
+            offset += 100;
+        }
 
-        // W Inputs
-        bindActions(panel, "W",
-                // Action for pressed key
-                new AbstractAction() {
-                    public void actionPerformed(ActionEvent e) {
-                        if (!wPressed) {
-                            wPressed = true;
-                            channels[0].noteOn(61, VELOCITY);
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(700, 350));
+        whiteKeysPanel.setBounds(0, 0, 700, 350);
+        blackKeysPanel.setBounds(0, 0, 700, 350);
+        layeredPane.add(whiteKeysPanel, JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(blackKeysPanel, JLayeredPane.PALETTE_LAYER);
 
-                        }
-                    }
-                },
-                // Action for released key
-                new AbstractAction() {
-                    public void actionPerformed(ActionEvent e) {
-                        System.out.println("A released");
-                        channels[0].noteOff(61);
-                        wPressed = false;
-                    }
-                }
-        );
 
-        // S Inputs
-        bindActions(panel, "S",
-                // Action for pressed key
-                new AbstractAction() {
-                    public void actionPerformed(ActionEvent e) {
-                        if (!sPressed) {
-                            sPressed = true;
-                            channels[0].noteOn(62, VELOCITY);
-                            sButton.setBackground(Color.GRAY);
-                        }
-                    }
-                },
-                // Action for released key
-                new AbstractAction() {
-                    public void actionPerformed(ActionEvent e) {
-                        channels[0].noteOff(62);
-                        sPressed = false;
-                        sButton.setBackground(Color.WHITE);
-                    }
-                }
-        );
+        this.add(layeredPane,BorderLayout.CENTER);
+        this.pack();
 
-        // A Inputs
-        bindActions(panel, "D",
-                // Action for pressed key
-                new AbstractAction() {
-                    public void actionPerformed(ActionEvent e) {
-                        if (!dPressed) {
-                            dPressed = true;
-                            channels[0].noteOn(64, VELOCITY);
-                            dButton.setBackground(Color.GRAY);
-                        }
-                    }
-                },
-                // Action for released key
-                new AbstractAction() {
-                    public void actionPerformed(ActionEvent e) {
-                        channels[0].noteOff(64);
-                        dPressed = false;
-                        dButton.setBackground(Color.WHITE);
-                    }
-                }
-        );
+        int noteValue = 60;
+        int counter = 0;
+        for(String key : pressedKeysWhite){
 
+            String pressedValue = key + "pressed";
+            pressedBooleans.put(pressedValue,false);
+            bindActionsToKeys(whiteKeysPanel,key,noteValue,WHITEKEYS[counter],pressedValue);
+
+            // If note is E, increment midi value with 1 , else with 2
+            if(noteValue == 64) {
+                noteValue += 1;
+            } else {
+                noteValue += 2;
+            }
+
+            counter++;
+        }
+        noteValue = 61;
+        counter = 0;
+        for(String key : pressedKeysBlack){
+
+            String pressedValue = key + "pressed";
+            pressedBooleans.put(pressedValue,false);
+            bindActionsToKeys(blackKeysPanel,key,noteValue,BLACKKEYS[counter],pressedValue);
+            
+            // If note is D#, increment midi value with 3 , else with 2
+            if(noteValue == 63) {
+                noteValue += 3;
+            } else {
+                noteValue += 2;
+            }
+            counter++;
+        }
 
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
@@ -186,6 +180,38 @@ public class Synth extends JFrame  {
         component.getInputMap(IFW).put(KeyStroke.getKeyStroke("released " + key),released);
         component.getActionMap().put(released,releasedAction);
 
+    }
+
+    private void bindActionsToKeys(JPanel panel, String key, int noteValue, String note, String pressedValue) {
+
+        Color defaultColor;
+        if(Arrays.asList(BLACKKEYS).contains(key))
+            defaultColor = Color.BLACK;
+        else
+            defaultColor = Color.WHITE;
+
+        bindActions(panel, key,
+            // Action for pressed key
+            new AbstractAction() {
+                public void actionPerformed(ActionEvent e) {
+                    if (pressedBooleans.get(pressedValue) == false) {
+                        pressedBooleans.put(pressedValue,true);
+                        channels[0].noteOn(noteValue, VELOCITY);
+                        JButton key = noteButtons.get(note);
+                        key.setBackground(Color.GRAY);
+                    }
+                }
+            },
+            // Action for released key
+            new AbstractAction() {
+                public void actionPerformed(ActionEvent e) {
+                    channels[0].noteOff(noteValue);
+                    pressedBooleans.put(pressedValue,false);
+                    JButton key = noteButtons.get(note);
+                    key.setBackground(defaultColor);
+                }
+            }
+        );
     }
 
 }
--- app/src/main/java/synthesizer/keys/WhiteKey.java
+++ app/src/main/java/synthesizer/keys/WhiteKey.java
@@ -6,28 +6,10 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class WhiteKey extends JButton implements KeyListener {
+public class WhiteKey extends JButton {
 
-    public WhiteKey(String b) {
+    public WhiteKey(String b,int[] size) {
         super(b);
     }
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        System.out.println("Pressed button");
-        if(e.getKeyCode() == KeyEvent.VK_A) {
-            System.out.println("A pressed!!");
-        }
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
 }
